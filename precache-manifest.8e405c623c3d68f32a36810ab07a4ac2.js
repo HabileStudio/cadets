667484cee8eef9ea74ca7a41eae1049aa90c226b
Henri <henri@habilestudio.com>
2019-03-13 11:30:58 +0100

    Updates

---
+++ precache-manifest.8e405c623c3d68f32a36810ab07a4ac2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d2b0d7ad7b0df9af40b",
+    "url": "/cadets/static/css/main.f7fa6261.chunk.css"
+  },
+  {
+    "revision": "8d2b0d7ad7b0df9af40b",
+    "url": "/cadets/static/js/main.9a360765.chunk.js"
+  },
+  {
+    "revision": "8ab7f4ce7e03fb967e59",
+    "url": "/cadets/static/js/runtime~main.f5a59821.js"
+  },
+  {
+    "revision": "8b3aa800dff5292c13de",
+    "url": "/cadets/static/js/2.f6a0c177.chunk.js"
+  },
+  {
+    "revision": "a0e330aab5f9e5fb9c5808f7ba066e5a",
+    "url": "/cadets/static/media/logo.a0e330aa.svg"
+  },
+  {
+    "revision": "31d761415390a52a9852fb1d774cf410",
+    "url": "/cadets/static/media/radar.31d76141.svg"
+  },
+  {
+    "revision": "43ccbcdb3f7f5472dd0314988eddd4a7",
+    "url": "/cadets/static/media/loupe.43ccbcdb.svg"
+  },
+  {
+    "revision": "c4a264ecdc0e623bc122b32cabeb48ba",
+    "url": "/cadets/index.html"
+  }
+];
